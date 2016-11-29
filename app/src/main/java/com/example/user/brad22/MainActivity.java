0deb8de8dcbc665aa4fff16eee54ae6097a1c780
Brad Chao <brad@brad.tw>
2016-11-29 12:08:04 +0800

    v3

--- app/src/main/java/com/example/user/brad22/MainActivity.java
+++ app/src/main/java/com/example/user/brad22/MainActivity.java
@@ -7,6 +7,7 @@ import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import java.util.List;
 
@@ -14,11 +15,17 @@ public class MainActivity extends AppCompatActivity {
     private SensorManager smgr;
     private Sensor sensor;
     private MySensorListener listener;
+    private TextView v0, v1, v2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        v0 = (TextView)findViewById(R.id.v0);
+        v1 = (TextView)findViewById(R.id.v1);
+        v2 = (TextView)findViewById(R.id.v2);
+
 
         smgr = (SensorManager)getSystemService(SENSOR_SERVICE);
 
@@ -37,12 +44,23 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public void onSensorChanged(SensorEvent sensorEvent) {
-
+            float[] values = sensorEvent.values;
+            v0.setText("X: "+values[0]);
         }
 
         @Override
         public void onAccuracyChanged(Sensor sensor, int i) {}
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        smgr.registerListener(listener,sensor,SensorManager.SENSOR_DELAY_NORMAL);
+    }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        smgr.unregisterListener(listener);
+    }
 }
