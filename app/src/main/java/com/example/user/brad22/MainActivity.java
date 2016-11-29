b0ea34bf12d9783c8e35cab57f3ee0fbbe8f62b4
Brad Chao <brad@brad.tw>
2016-11-29 13:59:45 +0800

    v4

--- app/src/main/java/com/example/user/brad22/MainActivity.java
+++ app/src/main/java/com/example/user/brad22/MainActivity.java
@@ -1,5 +1,9 @@
 package com.example.user.brad22;
 
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -7,6 +11,7 @@ import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
 import java.util.List;
@@ -15,17 +20,14 @@ public class MainActivity extends AppCompatActivity {
     private SensorManager smgr;
     private Sensor sensor;
     private MySensorListener listener;
-    private TextView v0, v1, v2;
+    private MyView myView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        v0 = (TextView)findViewById(R.id.v0);
-        v1 = (TextView)findViewById(R.id.v1);
-        v2 = (TextView)findViewById(R.id.v2);
-
+        //setContentView(R.layout.activity_main);
+        myView = new MyView(this);
+        setContentView(myView);
 
         smgr = (SensorManager)getSystemService(SENSOR_SERVICE);
 
@@ -40,12 +42,42 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    private class MyView extends View {
+        private boolean isInit;
+        private int viewW, viewH;
+        private Paint paintH, paintV, paintBall;
+        private float ballX, ballY,ballR;
+
+        public MyView(Context context) {
+            super(context);
+            setBackgroundColor(Color.BLACK);
+        }
+
+        private void init(){
+            viewW = getWidth(); viewH = getHeight();
+            paintH = new Paint(); paintH.setColor(Color.RED);
+            paintV = new Paint(); paintV.setColor(Color.BLUE);
+            paintBall = new Paint(); paintBall.setColor(Color.YELLOW);
+            ballX = viewW/2f; ballY = viewH/2f; ballR = 50;
+        }
+
+
+        @Override
+        protected void onDraw(Canvas canvas) {
+            super.onDraw(canvas);
+            if (!isInit) init();
+
+            canvas.drawCircle(ballX, ballY, ballR, paintBall);
+            canvas.drawLine(0,viewH/2f,viewW,viewH/2f,paintH);
+            canvas.drawLine(viewW/2f,0,viewW/2f,viewH,paintV);
+        }
+    }
+
     private class MySensorListener implements SensorEventListener {
 
         @Override
         public void onSensorChanged(SensorEvent sensorEvent) {
             float[] values = sensorEvent.values;
-            v0.setText("X: "+values[0]);
         }
 
         @Override
